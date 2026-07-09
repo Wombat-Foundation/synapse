c6073c84e8322292b347f0ce87fb7ea780c7265c
Shane Jaroch <chown_tee@proton.me>
2026-07-08 22:52:41 -0400

    state res: avoid duplicate auth extraction

--- rust/src/state_res.rs
+++ rust/src/state_res.rs
@@ -35,7 +35,8 @@ pub fn get_auth_chain_difference_from_event_graph<'py>(
     for (k, v) in event_map.iter() {
         let event_id: String = k.extract()?;
         let auth_ids: Vec<String> = if let Ok(event) = v.extract::<PyRef<Event>>() {
-            event.auth_event_ids()?
+            let data = event.resolver_data()?;
+            data.auth_events
         } else {
             v.call_method0("auth_event_ids")?.extract()?
         };
